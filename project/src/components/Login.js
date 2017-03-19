531491acdecd2f06db216f3ba6ba683f5b4dd2c2
simon <ertel.simon+github@gmail.com>
2017-03-19 22:15:29 +0100

    Add some providers
    Tentativ of adding providers with oauth - still miss Instagram

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -7,7 +7,7 @@ import hello from 'hellojs'
 import twitter from "./../assets/pics/twitter.svg"
 import facebook from "./../assets/pics/facebook.svg"
 import google from "./../assets/pics/google.svg"
-import insta from "./../assets/pics/insta.svg"
+import instagram from "./../assets/pics/insta.svg"
 import linkedin from "./../assets/pics/linkedIn.svg"
 
 class Login extends Component {
@@ -15,55 +15,81 @@ class Login extends Component {
     super();
 
     this.state = {
-
+      askForLogin: true
     };
 
-  }
-
-  componentDidMount(){
-      hello.init({
-          linkedin: '770iavt02pbb0b'
-      });
-
-  }
-
-  //https://github.com/MrSwitch/hello.js/issues/417
-  login(socialnet) {
-
-    let self = this;
-    hello(socialnet).login(
-        {
-            scope: 'repo,user'
-        }
-    ).then(function() {
-        return hello(socialnet).api('me');
-    })
-    .then(function(p) {
-        self.setState({user: p, open: false});
-        console.log(self.state);
+    hello.init({
+        facebook: '1181813181868792',
+        github: '90d108538ef1165dbfe5',
+        google: '1002056275138-02jqpiqacnas2rhebmitm249kqhfksdq.apps.googleusercontent.com',
+        linkedin: '770iavt02pbb0b',
+        twitter:  'j9ACbprzQXd6AEMvOUFGlEHov'
     });
 
+/*
+    // Facebook
+    // https://developers.facebook.com/apps/1181813181868792/settings/
+    // 1181813181868792
+
+    // Linkedin:
+    // https://www.linkedin.com/developer/apps/4706473/auth
+    // 770iavt02pbb0b
+
+    // Github
+    // https://github.com/settings/applications/348790
+    // 90d108538ef1165dbfe5
+
+    // Google
+    // https://console.developers.google.com/apis/credentials/oauthclient/1002056275138-02jqpiqacnas2rhebmitm249kqhfksdq.apps.googleusercontent.com?project=paris-museum
+    // 90d108538ef1165dbfe5
+
+    // Twitter
+    // https://apps.twitter.com/
+    // 90d108538ef1165dbfe5
+
+*/
 
   }
+
+
+
+  login(loginTo) {
+    console.log(loginTo);
+
+    const objHello = hello.use(loginTo);
+
+    objHello.api("me").then(function(r){
+      this.setState({askForLogin: false});
+    }.bind(this), function(e) {
+      this.setState({askForLogin: true});
+    }.bind(this));
+
+    const options = { display: "popup"};
+    const cb = () => {
+      console.log("Login callback");
+    }
+    hello.login(loginTo, options, cb);
+  }
+
 
   render(){
 	    return(
         <form role="form">
           <ul>
-              <li onClick={this.login.bind("linkedin")}>
+              <li onClick={()=>this.login('facebook')}>
                 <img alt="facebook" width="20" height="20" src={facebook} />
               </li>
-              <li>
-                <img alt="Twitter" width="20" height="20" src={twitter} />
+              <li onClick={()=>this.login('twitter')}>
+                <img alt="twitter" width="20" height="20" src={twitter} />
               </li>
-              <li>
+              <li onClick={()=>this.login('google')}>
                 <img alt="google" width="20" height="20" src={google} />
               </li>
               <li>
-                <img alt="insta" width="20" height="20" src={insta} />
+                <img alt="insta" width="20" height="20" src={instagram} />
               </li>
               <li>
-                <img onClick={this.login.bind("linkedin")} alt="LinkedIn" width="20" height="20" src={linkedin} />
+                <img onClick={()=>this.login('linkedin')} alt="LinkedIn" width="20" height="20" src={linkedin} />
               </li>
           </ul>
 
